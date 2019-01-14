93049fcd3da7b2e49f534ffd5f3bb2aa6731dc7f
Merline Belizaire <merline@noukod.com>
2019-01-14 19:54:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ get "/goodbye/:name" do
 end
 
 get "/multiply/:num1/:num2" do
-    @result=params[:num1].to_i*params[:num2].to_i
+    @result=@params[:num1].to_i*@params[:num2].to_i
     "#{@result}"
   end
 end
