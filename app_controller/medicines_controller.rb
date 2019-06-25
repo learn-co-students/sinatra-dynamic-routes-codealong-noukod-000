365d3ebcc5d234f7db8676ebd1ab727a0552ae25
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-25 14:10:04 +0000

    Done.

---
+++ app_controller/medicines_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < Sinatra::Base
+# medicines_controller.rb
+get '/medicines/:id' do
+  @medicine = all_the_medicines.select do |medicine|
+    medicine.id == params[:id]
+  end.first
+  erb :'/medicines/show.html'
+end
+end
